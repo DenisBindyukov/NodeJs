b062f8be74128d9182769a9a75863fab03ee4a25
DenisBindyukov <gitgroom@gmail.com>
2021-11-21 02:09:29 +0300

    new commit

---
+++ user.js
@@ -0,0 +1,16 @@
+const user = {
+    name: 'Katya',
+    age: 20
+}
+
+const user2 = {
+    name: 'Denis',
+    age: 20
+}
+
+module.exports = {
+    user: user,
+    sayHello() {
+        console.log('Hello')
+    }
+}
