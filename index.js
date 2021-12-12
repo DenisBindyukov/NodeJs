2bf15b08c1f3d5adfcc68c03164c480a77f28ae5
DenisBindyukov <gitgroom@gmail.com>
2021-12-12 20:14:11 +0300

    new commit

--- index.js
+++ index.js
@@ -1,22 +1,62 @@
 //Настройки сервера
 
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
-const  server = http.createServer((req, res) => {
-    console.log(req.url)
+const server = http.createServer((req, res) => {
 
-    //Отпровляем респонс
-    res.write('<h1>Hello from NodeJs</h1>')
-    res.write('<h2>Hello from NodeJs</h2>')
-    res.write('<h3>Hello from NodeJs</h3>')
-    res.write('<h4>Hello from NodeJs</h4>')
-    //Закрываем запрос
-    res.end(
-        `
-        <div style="background: blue; width: 200px; height: 200px">
-        <h1>Test</h1>
-</div>`
-    )
+    if (req.method === 'GET') {
+        res.writeHead(200, {
+            'Content-type': 'text/html; charset=utf-8'
+        })
+
+        if (req.url === '/') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'index.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err
+                    }
+
+                    res.end(content)
+                }
+            )
+        } else if (req.url === '/about') {
+            fs.readFile(
+                path.join(__dirname, 'views' , 'about.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err
+                    }
+
+                    res.end(content)
+                }
+            )
+        }
+
+        res.end();
+    } else if (req.method === 'POST') {
+        const body = [];
+        res.writeHead(200, {
+            'Content-type': 'text/html; charset=utf-8'
+        })
+
+        req.on('data', data => {
+            body.push(Buffer.from(data))
+        })
+
+        req.on('end', () => {
+            const message = body.toString().split('=')[1]
+
+            res.end(
+                `<h1>Ваше сообщение: ${message}</h1>`
+            );
+        })
+    }
+
 })
 
 
